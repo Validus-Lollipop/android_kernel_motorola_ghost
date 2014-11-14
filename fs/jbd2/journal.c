b131e479a894f03d939912019731b455b8200713
John Brewer <gzroms@hotmail.com>
2014-11-13 19:45:30 -0500

    Revert "Linaro 4.8 fixes"
    This reverts commit 89b5290a1e2252eabe4ec1f457c618dfb5775cbb.

--- fs/jbd2/journal.c
+++ fs/jbd2/journal.c
@@ -667,7 +667,7 @@ int jbd2_log_wait_commit(journal_t *journal, tid_t tid)
 
 int jbd2_journal_next_log_block(journal_t *journal, unsigned long long *retp)
 {
-	unsigned long blocknr = 0;
+	unsigned long blocknr;
 
 	write_lock(&journal->j_state_lock);
 	J_ASSERT(journal->j_free > 1);
